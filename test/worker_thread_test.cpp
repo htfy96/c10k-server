e8be85ed1e60fb47415917b17260027b3c2d7c39
Vic Luo <vicluo96@gmail.com>
2017-02-03 22:56:56 +0800

    Test: WorkerThread: Print backtrace when exception is thrown

--- test/worker_thread_test.cpp
+++ test/worker_thread_test.cpp
@@ -10,6 +10,9 @@
 #include <cstdint>
 #include <sstream>
 #include <iostream>
+#include <cstdio>
+#include <execinfo.h>
+#include <unistd.h>
 
 struct ServerHandler: public c10k::Handler
 {
@@ -111,7 +114,7 @@ TEST_CASE("multiple WorkerThread test", "[worker_thread]")
     using detail::call_must_ok;
     spdlog::set_level(spdlog::level::debug);
     auto server_logger = spdlog::stdout_color_mt("Server"), client_logger = spdlog::stdout_color_mt("Client"),
-        debug_logger = spdlog::stdout_color_mt("DEBUG");
+        debug_logger = spdlog::stdout_color_mt("DEBUG"), acceptor_logger(spdlog::stdout_color_mt("Acceptor"));
     detail::WorkerThread<ServerHandler> worker(400, server_logger);
 
     std::thread server_t {std::ref(worker)};
@@ -127,7 +130,7 @@ TEST_CASE("multiple WorkerThread test", "[worker_thread]")
         {
             int new_sock = call_must_ok(accept, "Accept", main_fd, nullptr, nullptr);
             make_socket_nonblocking(new_sock);
-            server_logger->info("new fd created = {}", new_sock);
+            acceptor_logger->info("new fd created = {}", new_sock);
             worker.add_new_connection(new_sock);
         }
         call_must_ok(::close, "close", main_fd);
@@ -142,14 +145,22 @@ TEST_CASE("multiple WorkerThread test", "[worker_thread]")
     client_t.detach();
     debug_logger->info("ClientWorker created");
 
-    for (int i=0; i<TEST_CNT; ++i)
+    try {
+        for (int i = 0; i < TEST_CNT; ++i) {
+            int cli_fd = create_socket(true);
+            sockaddr_in addr = create_addr("127.0.0.1", 6503);
+            call_must_ok(::connect, "Connect", cli_fd, (sockaddr *) &addr, sizeof(addr));
+            debug_logger->info("New fd added {}: {}/{}", cli_fd, i, TEST_CNT);
+            make_socket_nonblocking(cli_fd);
+            clientWorker.add_new_connection(cli_fd);
+        }
+    } catch(const std::exception &e)
     {
-        int cli_fd = create_socket(true);
-        sockaddr_in addr = create_addr("127.0.0.1", 6503);
-        call_must_ok(::connect, "Connect", cli_fd, (sockaddr*)&addr, sizeof(addr));
-        debug_logger->info("New fd added {}: {}/{}", cli_fd, i, TEST_CNT);
-        make_socket_nonblocking(cli_fd);
-        clientWorker.add_new_connection(cli_fd);
+        debug_logger->critical("Critical exception throw: {}", e.what());
+        void *bt[40];
+        int size = ::backtrace(bt, 40);
+        ::backtrace_symbols_fd(bt, size, STDERR_FILENO);
+        throw e;
     }
 
     cur_sleep_for(25s);
