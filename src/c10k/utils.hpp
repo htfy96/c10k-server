08e229f375aac2f059602bf473b3bdf5ac0bd289
Vic Luo <vicluo96@gmail.com>
2017-02-20 22:35:05 +0800

    Utils: Move make_socket_nonblocking, make_socket, make_addr into utils.hpp

--- src/c10k/utils.hpp
+++ src/c10k/utils.hpp
@@ -15,6 +15,12 @@
 #include <system_error>
 #include <utility>
 #include <type_traits>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include "endian.hpp"
 
 namespace c10k
 {
@@ -101,6 +107,38 @@ namespace c10k
             static constexpr bool value = type::value; \
         };
 
+        inline void make_socket_nonblocking(int socketfd)
+        {
+            using namespace c10k::detail;
+            int flags = call_must_ok(fcntl, "F_GETFL", socketfd, F_GETFL, 0);
+            flags |= O_NONBLOCK;
+            call_must_ok(fcntl, "F_SETFL", socketfd, F_SETFL, flags);
+        }
+
+        inline int create_socket(bool blocking = true)
+        {
+            using namespace c10k::detail;
+            int socketfd = call_must_ok(::socket, "Create socket", AF_INET, SOCK_STREAM, 0);
+
+            static const int ENABLE = 1;
+            if (!blocking) {
+                make_socket_nonblocking(socketfd);
+            }
+            call_must_ok(setsockopt, "setsockopt(REUSEADDR)", socketfd, SOL_SOCKET, SO_REUSEADDR, &ENABLE, sizeof(ENABLE));
+
+            return socketfd;
+        }
+
+        inline sockaddr_in create_addr(const char *in_addr, int port)
+        {
+            using namespace c10k::detail;
+            using namespace c10k;
+            sockaddr_in addr;
+            addr.sin_family = AF_INET;
+            call_must_ok(inet_aton, "inet_aton", in_addr, &addr.sin_addr);
+            addr.sin_port = to_net16(port);
+            return addr;
+        }
 
     }
 
--- test/test_common.hpp
+++ test/test_common.hpp
@@ -8,44 +8,7 @@
 
 #include <c10k/utils.hpp>
 #include <c10k/endian.hpp>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
 #include <thread>
-inline void make_socket_nonblocking(int socketfd)
-{
-    using namespace c10k::detail;
-    int flags = call_must_ok(fcntl, "F_GETFL", socketfd, F_GETFL, 0);
-    flags |= O_NONBLOCK;
-    call_must_ok(fcntl, "F_SETFL", socketfd, F_SETFL, flags);
-}
-
-inline int create_socket(bool blocking = true)
-{
-    using namespace c10k::detail;
-    int socketfd = call_must_ok(::socket, "Create socket", AF_INET, SOCK_STREAM, 0);
-
-    static const int ENABLE = 1;
-    if (!blocking) {
-        make_socket_nonblocking(socketfd);
-    }
-    call_must_ok(setsockopt, "setsockopt(REUSEADDR)", socketfd, SOL_SOCKET, SO_REUSEADDR, &ENABLE, sizeof(ENABLE));
-
-    return socketfd;
-}
-
-inline sockaddr_in create_addr(const char *in_addr, int port)
-{
-    using namespace c10k::detail;
-    using namespace c10k;
-    sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    call_must_ok(inet_aton, "inet_aton", in_addr, &addr.sin_addr);
-    addr.sin_port = to_net16(port);
-    return addr;
-}
 
 inline void cur_sleep_for(std::chrono::milliseconds ms)
 {
@@ -57,5 +20,9 @@ void cur_sleep_until( const std::chrono::time_point<Clock,Duration>& sleep_time 
 {
     std::this_thread::sleep_until(sleep_time);
 };
+
+using c10k::detail::create_addr;
+using c10k::detail::create_socket;
+using c10k::detail::make_socket_nonblocking;
 
 #endif //C10K_SERVER_TEST_COMMON_HPP
